f53e447d6e185634b43eb0e0ec0b5b6c8d4968a2
colin350 <colin350@gmail.com>
2014-12-16 09:32:31 -0500

    Created the File
    You can now grab it through the bookmarklet

---
+++ AMPlug.js
@@ -0,0 +1,57 @@
+var message = "AMPlug now enabled!";
+var autowoot = 0;
+var autograb = 0;
+
+API.chatLog(message); 
+
+API.on(API.CHAT_COMMAND, custom);
+
+function custom(value) {
+    if (value == "/am help") {
+        API.chatLog("Type '/am help' for a list of commands");
+        API.chatLog("Type '/am info' for information about the plugin");       
+        API.chatLog("Type '/autowoot <on/off>' to enable or disable autowoot");
+        API.chatLog("Type '/autograb <on/off>' to enable autograb. If you enable this, please create a playlist called autograb");
+        
+    }
+    
+    if (value == "/am info") {
+        API.chatLog("AMPlug developed by Colin Rioux (ArdosMusic). For more info, check him out on twitter @ArdosMusic");   
+    }
+    
+    if (value == "/autowoot on") {
+        autowoot = 1;
+        $('#woot').click(); 
+        API.chatLog("Autowoot has been activated!");
+    }
+    
+    if (value == "/autowoot off") {
+        autowoot = 0;
+        API.chatLog("You will stop wooting after the current song");
+    }
+    
+    if (value == "/autograb on") {
+        autograb = 1;
+        $('#grab').click();
+        API.chatLog("Autograb has been activated!");
+    }
+    
+    if (value == "/autograb off") {
+        autograb = 0;
+        API.chatLog("You will stop grabbing after the current song");
+    }
+}
+
+API.on(API.ADVANCE, advancedj);
+
+function advancedj() {
+    if (autowoot = 1) {
+        $('#woot').click(); 
+    } 
+    
+    if (autograb = 1) {
+        $('#grab').click();   
+    }
+}
+
+
