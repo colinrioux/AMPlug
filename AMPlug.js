3721ec532a547f929eb12277637be9f655a235ec
colin350 <colin350@gmail.com>
2014-12-17 23:04:57 -0500

    Major additions
    Chat may be buggy still

--- AMPlug.js
+++ AMPlug.js
@@ -1,8 +1,7 @@
 /*
 
-  Copyrighted (c) 2014 by Colin Rioux (ArdosMusic)
+  Copyrighted (c) 2014 by Colin Rioux (ArdosMusic / doetrioux)
   Please do not modify or use without permission
-  Created as of Fall 2014
 
 */
 
@@ -17,14 +16,15 @@ var autograb = 0;
 
 
 
-API.chatLog(message);
-$('#chat .update .text').css("color", "#51A3C9");
-$('#chat .update .text').css("border-left", "#51A3C9 3px solid");
+//API.chatLog(message);
+loadSettings();
 
+Chat('init', message);
 
 API.on(API.CHAT_COMMAND, customcommand);
 
 function customcommand(value) {
+
     if (value == "/am help" || value == "/am") {
         API.chatLog("Type '/am help' for a list of commands");
         API.chatLog("Type '/am info' for information about the plugin");
@@ -70,4 +70,21 @@ function advancedj() {
     if (autograb = 1) {
         $('#grab').click();
     }
+}
+
+function Chat(type, dest) {
+
+  if ($('#chat-message').css('display') == 'block') {
+      var chat = $('#chat-messages'), a = chat.scrollTop() > chat[0].scrollHeight - chat.height() - 28;
+      chat.append('<div class="update am-' + type + '"><span class="text">' + dest + '</span></div>');
+
+  }
+
+}
+
+function loadSettings() {
+
+  $('head').append('<style type=text/css id=amplug-css> .am-init{border-left:3px solid #D1D119}#chat .update.am-init .text{color:#D1D119}');
+
+
 }
