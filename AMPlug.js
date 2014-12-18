0b14c1ff3ec074a21b7fd4f9e0dc7897cd85cf98
colin350 <colin350@gmail.com>
2014-12-17 23:14:00 -0500

    Organized Code Blocks

--- AMPlug.js
+++ AMPlug.js
@@ -1,25 +1,25 @@
 /*
 
-  Copyrighted (c) 2014 by Colin Rioux (ArdosMusic / doetrioux)
+  Copyrighted (c) 2014 by Colin Rioux (ArdosMusic)
   Please do not modify or use without permission
 
 */
 
+// Global Variables :
 
-
-
-var message = "AMPlug now enabled!";
+var message = "AMPlug now enabled! Do /am for a list of commands";
 var autowoot = 0;
-var autograb = 0;
 
 // $('#chat-messages').css("color","#E5E500"); <-- Example how to modify color
 
+startup();
 
+function startup() {
+  API.chatLog(message); // Temp. Bootup Message
 
-API.chatLog(message);
-loadSettings();
+}
 
-Chat('init', message);
+// Custom Commands :
 
 API.on(API.CHAT_COMMAND, customcommand);
 
@@ -29,7 +29,6 @@ function customcommand(value) {
         API.chatLog("Type '/am help' for a list of commands");
         API.chatLog("Type '/am info' for information about the plugin");
         API.chatLog("Type '/am autowoot <on/off>' to enable or disable autowoot");
-        API.chatLog("Type '/am autograb <on/off>' to enable autograb. If you enable this, please create a playlist called autograb");
 
     }
 
@@ -48,17 +47,9 @@ function customcommand(value) {
         API.chatLog("You will stop wooting after the current song");
     }
 
-    if (value == "/am autograb on") {
-        autograb = 1;
-        $('#grab').click();
-        API.chatLog("Autograb has been activated!");
-    }
-
-    if (value == "/am autograb off") {
-        autograb = 0;
-        API.chatLog("You will stop grabbing after the current song");
-    }
 }
+
+// Next DJ Advances:
 
 API.on(API.ADVANCE, advancedj);
 
@@ -66,26 +57,5 @@ function advancedj() {
     if (autowoot = 1) {
         $('#woot').click();
     }
-
-    if (autograb = 1) {
-        $('#grab').click();
-    }
-}
-
-function Chat(type, dest) {
-
-  if ($('#chat-message').css('display') == 'block') {
-      var chat = $('#chat-messages'), a = chat.scrollTop() > chat[0].scrollHeight - chat.height() - 28;
-      chat.append('<div class="update am-' + type + '"><span class="text">' + dest + '</span></div>');
-      if (a) chat.scrollTop(chat[0].scrollHeight);
-      if (chat.children().length >= 512) chat.children().first().remove();
-    } else API.chatLog(m.replace(/<br>/g,', ').replace(/<\/?span>/g,''),true);
-
-}
-
-function loadSettings() {
-
-  $('head').append('<style type=text/css id=amplug-css> .am-init{border-left:3px solid #D1D119}#chat .update.am-init .text{color:#D1D119}');
-
 
 }
