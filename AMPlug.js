d36f5685fc5db22e003a18844d4c8adadc8827ae
Colin Rioux <colin350@gmail.com>
2016-09-21 17:42:02 -0400

    Test file

--- AMPlug.js
+++ AMPlug.js
@@ -3,3 +3,22 @@
  *  Remodeled file
  *
  */
+
+const boot_message = "[AMPlug]: Plugin now enabled! Type '+am' for a list of commands.";
+
+boot();
+
+function boot() {
+    API.chatLog(boot_message);
+}
+
+API.on(API.CHAT_COMMAND, cHandler);
+
+function cHandler(value) {
+    let prefix = "+";
+    var cArr = value.split(" ");
+    if (value.startsWith("+am") && (cArr[1] === "help" || cArr.length == 1)) {
+        API.chatLog(["Test", "Test", "Test"].join("\n"));
+    }
+
+}
