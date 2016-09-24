9d07c5c7a1019bc40f60e3aa8b7a024c77d6ecb5
Colin Rioux <colin350@gmail.com>
2016-09-23 23:06:00 -0400

    Test

--- AMPlug.js
+++ AMPlug.js
@@ -9,14 +9,13 @@ var boot_message = "[AMPlug]: Plugin now enabled! Type '+am' for a list of comma
 boot();
 
 function boot() {
-    API.chatLog(boot_message, true);
+    API.chatLog(boot_message);
 }
 
 API.on(API.CHAT_COMMAND, cHandler);
 
 function cHandler(value) {
-    let prefix = "+";
-    var cArr = value.split(" ");
+   
     if (value == "/am") {
         API.chatLog("Test");
     }
